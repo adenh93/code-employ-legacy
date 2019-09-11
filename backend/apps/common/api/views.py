51fe011b2e4ae5eea3ff25ac3f975996957f1239
Aden Herold <aden.herold1@gmail.com>
2019-09-11 15:51:09 +0930

    LookupCodeListView get request is now cached for 60 minutes after the initial request.

--- backend/apps/common/api/views.py
+++ backend/apps/common/api/views.py
@@ -2,9 +2,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from ..business.selectors import ProgrammingLanguageSelector, LocationSelector
 from .serializers import ProgrammingLanguageSerializer, LookupCodeListSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
 class LookupCodeListView(APIView):
+    @method_decorator(cache_page(60*60))
     def get(self, request):
         languages = ProgrammingLanguageSelector.get_programming_languages()
         countries = LocationSelector.get_country_codes()
